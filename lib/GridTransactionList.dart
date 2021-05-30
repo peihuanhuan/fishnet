0de653136645c5ff067d528427bb4d413b8714bb
裴欢 <peihuan@meituan.com>
2021-05-31 01:46:32 +0800

    feat: on my way!

--- lib/GridTransactionList.dart
+++ lib/GridTransactionList.dart
@@ -5,8 +5,10 @@ import 'package:fishnet/util/CommonUtils.dart';
 import 'package:fishnet/util/CommonWight.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 
+import 'PopupMenu.dart';
 
 class GridTransactionList extends StatefulWidget {
   int _varietyId;
@@ -40,12 +42,15 @@ class _GridTransactionListState extends State<GridTransactionList> {
 
   @override
   Widget build(BuildContext context) {
-
-    if(_transactions.isEmpty) {
+    if (_transactions.isEmpty) {
       return Container(
         color: Colors.white,
         child: Center(
-          child: Text("还没有数据哦", textAlign: TextAlign.center, style: TextStyle(color: color1, fontSize: 12),),
+          child: Text(
+            "还没有数据哦",
+            textAlign: TextAlign.center,
+            style: TextStyle(color: color1, fontSize: 12),
+          ),
         ),
       );
     }
@@ -72,7 +77,11 @@ class _GridTransactionListState extends State<GridTransactionList> {
 
   Widget buildItemCard(int index) {
     var transaction = _transactions[index];
+    var cardGlobalKey = GlobalKey();
+
+    Offset globalPosition;
     Widget card = Card(
+      key: cardGlobalKey,
       color: cc[index % 4],
       child: ExpansionTile(
         tilePadding: EdgeInsets.fromLTRB(0, 0, 8, 0),
@@ -86,22 +95,55 @@ class _GridTransactionListState extends State<GridTransactionList> {
         leading: buildLeading("${transaction.level}"),
         title: buildTitle(transaction),
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: <Widget>[
-                buildKeyValuePair(
-                    "买入时间", yyyyMMddFormat.format(transaction.buy.time)),
-                buildKeyValuePair("持有天数", transaction.holdingDays()),
-                buildKeyValuePair("年化率",
-                    "${toPercentage(transaction.annualizedRate(widget._currentPrice))}"),
-                buildKeyValuePair("留存数量", transaction.retainedNumber()),
-                Icon(
-                  Icons.edit,
-                  color: color2,
-                ),
-                Icon(Icons.delete),
-              ],
+          GestureDetector(
+
+            // onTapUp: (TapUpDetails detail) {
+            //   //获取当前触摸点的全局坐标
+            //   globalPosition=detail.globalPosition;
+            //   print(globalPosition);
+            //   //获取当前触摸点的局部坐标
+            //   var localPosition=detail.localPosition;
+            // },
+            onTapUp: (TapUpDetails detail){
+
+              print(detail.globalPosition);
+              print(detail.globalPosition.dx);
+              showMenu(
+                  context: context,
+                  // position: RelativeRect.fromLTRB(100.0, 200.0, 100.0, 100.0),
+                  position: RelativeRect.fromLTRB(1, top(cardGlobalKey), 0, 0),
+                  items: <PopupMenuItem<String>>[
+                    new PopupMenuItem<String>( value: 'value01', child: new Text('Item One')),
+                    new PopupMenuItem<String>( value: 'value02', child: new Text('Item Two')),
+                  ] );
+
+              // showDialog(
+              //     context: context,
+              //     builder: (context) {
+              //       return _editorDialogBuilder(context, index);
+              //     });
+            },
+            onLongPress: (){
+
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return _deleteDialogBuilder(context, index);
+                  });
+
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: <Widget>[
+                  buildKeyValuePair(
+                      "买入时间", yyyyMMddFormat.format(transaction.buy.time)),
+                  buildKeyValuePair("持有天数", transaction.holdingDays()),
+                  buildKeyValuePair("年化率",
+                      "${toPercentage(transaction.annualizedRate(widget._currentPrice))}"),
+                  buildKeyValuePair("留存数量", transaction.retainedNumber()),
+                ],
+              ),
             ),
           ),
         ],
@@ -109,6 +151,108 @@ class _GridTransactionListState extends State<GridTransactionList> {
     );
 
     return card;
+  }
+
+  double top(GlobalKey<State<StatefulWidget>> cardGlobalKey) {
+
+    var findRenderObject = (cardGlobalKey.currentContext.findRenderObject() as RenderBox);
+
+    // print(findRenderObject.localToGlobal(Offset.zero));
+    // print(findRenderObject.size);
+
+
+    return findRenderObject.localToGlobal(Offset.zero).dy;
+  }
+
+  AlertDialog _editorDialogBuilder(BuildContext context, int index) {
+    var transaction = _transactions[index];
+
+    var buyNumber = transaction.buy.number;
+    var buyPrice = transaction.buy.price;
+    var sellNumber = transaction.sell.number;
+    var sellPrice = transaction.sell.price;
+
+    return AlertDialog(
+      title: Text("修改"),
+      content: Column(
+        children: [
+          buySellNumberTextField("买入数量", buyNumber, (number) => buyNumber = number),
+          buySellTextField("买入价格", buyPrice, (price) => buyPrice = price),
+
+          buySellNumberTextField("卖出数量", sellNumber, (number) => sellNumber = number),
+          buySellTextField("卖出价格", sellPrice, (price) => sellPrice = price),
+        ],
+      ),
+      actions: <Widget>[
+        TextButton(
+          child: Text("取消"),
+          onPressed: () => Navigator.of(context).pop(), //关闭对话框
+        ),
+        TextButton(
+          child: Text("删除"),
+          onPressed: () {
+            setState(() {
+              // todo 持久化
+              _transactions.removeAt(index);
+            });
+            Navigator.of(context).pop(true); //关闭对话框
+          },
+        ),
+      ],
+    );
+  }
+
+  TextField buySellNumberTextField(String title, int number, Function onChange) {
+    return TextField(
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(labelText: "买入数量"),
+          controller: TextEditingController()..text = number.toString(),
+          inputFormatters: <TextInputFormatter>[
+            FilteringTextInputFormatter.digitsOnly,
+            LengthLimitingTextInputFormatter(10)
+          ],
+          onChanged: (str) {
+            onChange(int.parse(str));
+          },
+        );
+  }
+
+  TextField buySellTextField(String title, num price, Function onChange) {
+    return TextField(
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(labelText: title),
+          controller: TextEditingController()..text = price.toString(),
+          inputFormatters: <TextInputFormatter>[
+            FilteringTextInputFormatter.allow(RegExp("[0-9.]")),
+            LengthLimitingTextInputFormatter(10)
+          ],
+          onChanged: (str) {
+            onChange(num.parse(str));
+          },
+        );
+  }
+
+  AlertDialog _deleteDialogBuilder(BuildContext context, int index) {
+    return AlertDialog(
+      title: Text("提示"),
+      content: Text("确定要删除该波段吗?"),
+      actions: <Widget>[
+        TextButton(
+          child: Text("取消"),
+          onPressed: () => Navigator.of(context).pop(), //关闭对话框
+        ),
+        TextButton(
+          child: Text("删除"),
+          onPressed: () {
+            setState(() {
+              // todo 持久化
+              _transactions.removeAt(index);
+            });
+            Navigator.of(context).pop(true); //关闭对话框
+          },
+        ),
+      ],
+    );
   }
 
   Flex buildTitle(TwoDirectionTransactions transaction) {
